b08ba8daca0e081c2348779c14a6d8b09e1d08e4
abraham okezie <abrahamsmooth41@gmail.com>
2022-10-21 21:15:03 -0700

    yea

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - program that finds and prints the largest prime factor of number N
+*
+* Return: void
+*/
+int main(void)
+{
+long N = 612852475143;
+int i;
+int m;
+for (i = 2; i <= N; i++)
+{
+m = N % i;
+if (m == 0)
+{
+N = N / i;
+i--;	  
+}
+}
+printf("%d\n", i); 
+return (0);
+}
