75e32e24f71af9bef6f95aafb9cdb7357b7fda42
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2022-10-25 12:47:17 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,12 @@
-jkjk
+#include "main.h"
+/**
+* _puts - write string followed by new line
+* @str: string
+* Return: void
+*/
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+_putchar('\n');
+}
