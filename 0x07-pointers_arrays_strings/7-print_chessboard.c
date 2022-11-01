c9db7dab3b71cf3a0d83b925d4663791aadb7ed3
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2022-11-01 03:24:22 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,16 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* print_chessboard - Entry point
-* @a: array
-* Return: Always 0 (Success)
+* print_chessboard - print a chessboard.
+* @a: source matrix
+*
+*
+* Return: chessboard.
 */
 void print_chessboard(char (*a)[8])
 {
-int i;
-int j;
-for (i = 0; i < 8; i++)
+int i, j;
+for (i = 0; i <= 7; i++)
 {
-for (j = 0; j < 8; j++)
+for (j = 0; j <= 7; j++)
+{
 _putchar(a[i][j]);
+}
 _putchar('\n');
+}
 }
