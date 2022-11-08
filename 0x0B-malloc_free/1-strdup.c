df4a6e84e187fb8845c77eada010eaf8a611c569
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2022-11-08 04:17:19 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,13 +13,15 @@ unsigned int i;
 int n;
 if (str == NULL)
 return (NULL);
-for (n = 0; str[n] != '\0'; n++);
+for (n = 0; str[n] != '\0'; n++)
+;
 strnew = (char *)malloc(n + 1 * sizeof(char));
 if (strnew != NULL)
 {
 for (i = 0; str[i] != '\0'; i++)
 strnew[i] = str[i];
-} else
+}
+else
 {
 return (NULL);
 }
