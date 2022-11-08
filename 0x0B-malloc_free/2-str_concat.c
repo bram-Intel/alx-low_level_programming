94b567225d06869c65789912fa2d5952be5a4b42
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2022-11-08 04:18:52 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,8 +19,10 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (n1 = 0; s1[n1] != '\0'; n1++);
-for (n2 = 0; s2[n2] != '\0'; n2++);
+for (n1 = 0; s1[n1] != '\0'; n1++)
+;
+for (n2 = 0; s2[n2] != '\0'; n2++)
+;
 strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
 if (strnew == NULL)
 {
