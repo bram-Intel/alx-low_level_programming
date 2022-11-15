23baef834d3c6ab882d4aa94d520e4cb0074c34a
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2022-11-15 02:36:23 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - list dates about pet
- * @name: pet name
- * @age: pet age
- * @owner: pet owner name
- */
+* struct dog - list dates about pet
+* @name: pet name
+* @age: pet age
+* @owner: pet owner name
+*/
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
