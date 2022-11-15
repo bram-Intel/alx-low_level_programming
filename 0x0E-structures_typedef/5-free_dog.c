11548660fe1355f87def1688628e638fd04606e2
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2022-11-15 02:34:02 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,15 @@
-ljkl
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog -entry point
+* @d: entry structure with pointer
+*/
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
