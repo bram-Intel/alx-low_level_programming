fca3fc070e74706bd619fd61d022aff093b1fc4f
Okezie Abraham <71279164+bram-Intel@users.noreply.github.com>
2023-01-13 04:18:49 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,12 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int elements = 0;
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		elements++;
-	}
-	return (elements);
+int elements = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+elements++;
+}
+return (elements);
 }
